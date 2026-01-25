772b16de9f053083821b2393c2797c3c054adae4
MarkinZal <zalubil.marek@gmail.com>
2026-01-25 15:02:34 +0100

    Oprava
    Oprava tutorialu.

--- Scripts/tutorial_logic.gd
+++ Scripts/tutorial_logic.gd
@@ -1,25 +1,40 @@
 extends Node2D
 
-@export var trees_to_cut_count : int = 3 
+var total_trees_count : int = 0
 var current_cut_trees : int = 0
 
-
-@onready var end_screen = $Scripts/end_screen
+@export var end_screen : Control 
 
 func _ready():
 	var trees = get_tree().get_nodes_in_group("Tree")
 	
+	total_trees_count = trees.size()
+	print("Celkem stromů na mapě: ", total_trees_count)
+	
+	if total_trees_count == 0:
+		print("Varování: Žádné stromy nenalezeny!")
+		finish_tutorial()
+		return
+
 	for tree in trees:
 		if tree.has_signal("tree_destroyed"):
 			tree.tree_destroyed.connect(_on_tree_destroyed)
 
 func _on_tree_destroyed():
 	current_cut_trees += 1
-	print("Pokáceno stromů: ", current_cut_trees)
+	print("Pokáceno: ", current_cut_trees, " / ", total_trees_count)
 	
-	if current_cut_trees >= trees_to_cut_count:
+	if current_cut_trees >= total_trees_count:
 		finish_tutorial()
 
 func finish_tutorial():
 	if end_screen:
-		end_screen.set_custom_message("Tutorial completed!")
+		if end_screen.has_method("set_custom_message"):
+			end_screen.set_custom_message("Tutorial Dokončen!")
+		else:
+			end_screen.visible = true
+			var label = end_screen.get_node_or_null("HeaderText")
+			if label:
+				label.text = "Tutorial Dokončen!"
+	else:
+		print("CHYBA: Není připojen EndScreen v Inspektoru!")
