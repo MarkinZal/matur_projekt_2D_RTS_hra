08962e4a7188e38e80f7ac4770f0149ff11081ec
MarkinZal <zalubil.marek@gmail.com>
2026-03-22 16:08:20 +0100

    idk
    debuggování

--- Scripts/unit_archer.gd
+++ Scripts/unit_archer.gd
@@ -1,13 +1,1 @@
-extends Unit
-class_name UnitArcher
-
-func _ready():
-	super._ready()
-	
-	move_speed = 40.0
-	health_max = 12
-	health_current = 12
-	
-	attack_range = 120.0
-	attack_rate = 1.0
-	attack_damage = 8
+extends Area2D
