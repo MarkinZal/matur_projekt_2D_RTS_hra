42e2a05e485a37f7289812672d679db4560a0851
MarkinZal <zalubil.marek@gmail.com>
2026-03-31 22:28:18 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH

--- Scripts/Units/enemy_commander.gd
+++ Scripts/Units/enemy_commander.gd
@@ -1,14 +1,22 @@
 extends Node
-class_name EnemyCommander
+class_name DefenderCommander
 
+@export_category("Základní Nastavení")
 @export var decision_interval: float = 3.0
-@export var max_army_size: int = 10
-@export var attack_wave_size: int = 5
-
-@export var enemy_barracks: Array[Node2D]
+@export var enemy_base: Node2D
 @export var player_base: Node2D
 
-var current_enemy_gold: int = 500
+@export_category("Budování a Ekonomika")
+@export var tower_scene: PackedScene 
+@export var tower_build_spots: Array[Marker2D] 
+@export var tower_cost: int = 150
+
+@export_category("Armáda")
+@export var enemy_barracks: Array[Node2D]
+@export var max_army_size: int = 20
+@export var unit_cost: int = 50
+
+var current_enemy_gold: int = 200
 var my_army: Array[Node2D] = []
 
 @onready var decision_timer: Timer = Timer.new()
@@ -21,35 +29,55 @@ func _ready():
 
 func _on_decision_tick():
 	update_army_list()
-	manage_economy_and_production()
-	manage_military_tactics()
+	manage_economy()
+	build_defenses()
+	train_army()
+	manage_tactics()
 
 func update_army_list():
-	my_army = get_tree().get_nodes_in_group("UnitAI")
+	my_army.assign(get_tree().get_nodes_in_group("UnitEnemy"))
 
-func manage_economy_and_production():
-	current_enemy_gold += 10 
-	
-	if my_army.size() < max_army_size and current_enemy_gold >= 50:
-		train_unit()
+func manage_economy():
+	current_enemy_gold += 15 
 
-func train_unit():
+func build_defenses():
+	if current_enemy_gold >= tower_cost and not tower_build_spots.is_empty():
+		var build_spot = tower_build_spots.pop_front()
+		
+		if is_instance_valid(build_spot) and tower_scene:
+			var new_tower = tower_scene.instantiate()
+			get_parent().add_child(new_tower) 
+			new_tower.global_position = build_spot.global_position
+			current_enemy_gold -= tower_cost
+			
+			new_tower.add_to_group("BuildingEnemy")
+
+func train_army():
 	if enemy_barracks.is_empty(): return
 	
-	var spawner = enemy_barracks.pick_random()
-	if spawner.has_method("spawn_unit"):
-		spawner.spawn_unit()
-		current_enemy_gold -= 50
+	if my_army.size() < max_army_size and current_enemy_gold >= unit_cost:
+		var spawner = enemy_barracks.pick_random()
+		if is_instance_valid(spawner) and spawner.has_method("spawn_unit"):
+			spawner.spawn_unit()
+			current_enemy_gold -= unit_cost
 
-func manage_military_tactics():
+func manage_tactics():
 	var idle_army = get_idle_units()
 	
-	if idle_army.size() >= attack_wave_size and is_instance_valid(player_base):
+	if my_army.size() >= max_army_size and is_instance_valid(player_base):
 		for unit in idle_army:
 			var ai_node = unit.get_node_or_null("UnitAI")
 			if ai_node:
 				ai_node.target_node = player_base
 				ai_node.change_state(ai_node.State.CHASE)
+	else:
+		if is_instance_valid(enemy_base):
+			for unit in idle_army:
+				if unit.global_position.distance_to(enemy_base.global_position) > 150.0:
+					var ai_node = unit.get_node_or_null("UnitAI")
+					if ai_node:
+						ai_node.target_node = enemy_base
+						ai_node.change_state(ai_node.State.CHASE)
 
 func get_idle_units() -> Array[Node2D]:
 	var idle_units: Array[Node2D] = []
